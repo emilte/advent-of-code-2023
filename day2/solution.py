1bcc0cd3c59c95d3fc2bbf90e8501ca3dfe10be7
Emil Telstad <22004178+emilte@users.noreply.github.com>
2023-12-02 22:34:25 +0100

    Add solution to day 2.

--- day2/solution.py
+++ day2/solution.py
@@ -1,37 +1,79 @@
 """
 https://adventofcode.com/2023/day/2
 """
-from time import perf_counter_ns
-from functools import wraps
 
+from utils import timeit
 from task_input import task_input
 
-
-def timeit(func):
-
-    @wraps(func)
-    def decorator(*args, **kwargs):
-        start = perf_counter_ns()
-        func(*args, **kwargs)
-        end = perf_counter_ns()
-        ms = (end - start) / 1000 / 1000
-        print(f'{ms:.1f} ms')
-
-    return decorator
+RED = 'red'
+GREEN = 'green'
+BLUE = 'blue'
 
 
 @timeit
-def part_1() -> None:
+def part_1(input) -> None:
     """Solution to part 1."""
-    ...
+
+    limits = {
+        RED: 12,
+        GREEN: 13,
+        BLUE: 14,
+    }
+
+    s = 0
+    lines = input.strip().split('\n')
+    for line in lines:
+        # (Game 6: 1 red, 12 blue; 20 blue, 3 green, 2 red; 4 red, 15 blue)
+        game, sets_str = line.split(': ', maxsplit=1)
+        # (Game 6) (1 red, 12 blue; 20 blue, 3 green, 2 red; 4 red, 15 blue)
+        game_id = int(game.split(' ', maxsplit=1)[1])
+        sets = sets_str.split(';')  # (1 red, 12 blue) (20 blue, 3 green, 2 red) (4 red, 15 blue)
+
+        valid = True
+        for set in sets:
+            # (1 red, 12 blue)
+            set_cubes = set.strip().split(', ')  # (1 red) (12 blue)
+
+            for cubes in set_cubes:
+                c, t = cubes.split(' ')  # (1) (red)
+
+                if int(c) > limits[t]:
+                    valid = False
+                    break
+        if valid:
+            s += game_id
+
+    print(s)
 
 
 @timeit
-def part_2() -> None:
+def part_2(input: str) -> None:
     """Solution to part 2."""
-    ...
+    sum_power = 0
+    lines = input.strip().split('\n')
+    for line in lines:
+        # (Game 6: 1 red, 12 blue; 20 blue, 3 green, 2 red; 4 red, 15 blue)
+        sets_str = line.split(': ', maxsplit=1)[1]
+        # (1 red, 12 blue; 20 blue, 3 green, 2 red; 4 red, 15 blue)
+        sets = sets_str.split(';')  # (1 red, 12 blue) (20 blue, 3 green, 2 red) (4 red, 15 blue)
+
+        maks = {RED: 0, GREEN: 0, BLUE: 0}
+        for set in sets:
+            # (1 red, 12 blue)
+            set_cubes = set.strip().split(', ')  # (1 red) (12 blue)
+
+            for cubes in set_cubes:
+                c, t = cubes.split(' ')  # (1) (red)
+
+                if int(c) > maks[t]:
+                    # New max discovered, store it.
+                    maks[t] = int(c)
+
+        sum_power += maks[RED] * maks[GREEN] * maks[BLUE]
+
+    print(sum_power)
 
 
 if __name__ == '__main__':
-    part_1()
-    part_2()
+    part_1(task_input)
+    part_2(task_input)
