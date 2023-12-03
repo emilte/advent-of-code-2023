50e4d0a9c50d7c7d183bf1e3323fff7f4f27f38d
Emil Telstad <22004178+emilte@users.noreply.github.com>
2023-12-03 18:53:33 +0100

    Add solution to day 3.

--- day3/solution.py
+++ day3/solution.py
@@ -1,37 +1,103 @@
 """
 https://adventofcode.com/2023/day/3
 """
-from time import perf_counter_ns
-from functools import wraps
+from math import prod
 
-from task_input import task_input
+from utils import timeit
+from task_input import task_input, sample
 
 
-def timeit(func):
+def is_symbol(l: str) -> bool:
+    return not l.isdigit() and l != '.'
 
-    @wraps(func)
-    def decorator(*args, **kwargs):
-        start = perf_counter_ns()
-        func(*args, **kwargs)
-        end = perf_counter_ns()
-        ms = (end - start) / 1000 / 1000
-        print(f'{ms:.1f} ms')
 
-    return decorator
+def scan_symbol(lines: list[str], i: int, j: int) -> bool:
+    """Determine if symbol is adjecent."""
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if x == 0 and y == 0:
+                continue
+            m = i + y
+            n = j + x
+            l = lines[m][n]
+            if is_symbol(l):
+                True
+    return False
+
+
+def find_star(lines: list[str], i: int, j: int) -> tuple[bool, int, int]:
+    """Determine location of possible adjecent star."""
+    for y in range(-1, 2):
+        for x in range(-1, 2):
+            if x == 0 and y == 0:
+                continue
+            m = i + y
+            n = j + x
+            l = lines[m][n]
+            if l == '*':
+                return True, m, n
+    return False, -1, -1
 
 
 @timeit
-def part_1() -> None:
+def part_1(task_input) -> None:
     """Solution to part 1."""
-    ...
+    lines = task_input.strip().split('\n')
+    border = ['.' * len(lines[0])]
+    lines = border + lines + border
+    lines = [f'..{line}..' for line in lines]
+
+    s = 0
+    for i, line in enumerate(lines):
+        n = ''
+        keep = False
+        for j, l in enumerate(line):
+
+            if l.isdigit():
+                n += l
+                if scan_symbol(lines, i, j):
+                    keep = True
+
+            if n and not l.isdigit():
+                if keep:
+                    s += int(n)
+                n = ''
+                keep = False
+    print(s)
 
 
 @timeit
-def part_2() -> None:
+def part_2(task_input) -> None:
     """Solution to part 2."""
-    ...
+    lines = task_input.strip().split('\n')
+    border = ['.' * len(lines[0])]
+    lines = border + lines + border
+    lines = [f'..{line}..' for line in lines]
+
+    d = {}
+
+    for i, line in enumerate(lines):
+        n = ''
+        yx = ''
+        for j, l in enumerate(line):
+
+            if l.isdigit():
+                n += l
+                found, y, x = find_star(lines, i, j)
+                if found:
+                    yx = f'{y}-{x}'
+                    d.setdefault(yx, [])
+
+            if n and not l.isdigit():
+                if yx:
+                    d[yx].append(int(n))
+                n = ''
+                yx = ''
+
+    s = sum([prod(x) for x in d.values() if len(x) == 2])
+    print(s)
 
 
 if __name__ == '__main__':
-    part_1()
-    part_2()
+    part_2(task_input)
+    part_2(task_input)
